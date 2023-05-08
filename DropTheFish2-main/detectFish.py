1336d6b0c37d23ff79a0697446467ce0f2416699
storyfor980107 <storyfor980107@naver.com>
2023-05-08 18:42:46 +0900

    Feat: show all detected fish list

--- DropTheFish2-main/detectFish.py
+++ DropTheFish2-main/detectFish.py
@@ -74,7 +74,7 @@ def clear_confidence_list():
   confidence_list.clear()
 def get_best_fish():
   print('현재 어종별 확률 : ',confidence_list)
-  best_confidence = max(confidence_list)
+  best_confidence = (confidence_list)
   index = confidence_list.index(best_confidence)
   print('확률 높은 어종 : ', final_result[index], confidence_list[index])
   return final_result[index], confidence_list[index]
